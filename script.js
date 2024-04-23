221ac3b510d25adbeb0b34d3c926fba94f31c55d
Md Jubayer <63995667+Jubayer909@users.noreply.github.com>
2024-04-23 12:56:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-import VerifiedRoundedIcon from '@mui/icons-material/VerifiedRounded';
-
-document.getElementById('facebook').addEventListener('click', function() {
-  window.open('https://www.facebook.com/share?url=' + encodeURIComponent(window.location.href), '_blank');
+// Add event listeners to social media icons
+document.querySelectorAll('.icon').forEach(function(icon) {
+  icon.addEventListener('click', function(event) {
+    event.preventDefault();
+    var platform = this.id;
+    switch (platform) {
+      case 'facebook':
+        window.open('https://www.facebook.com/', '_blank');
+        break;
+      case 'twitter':
+        window.open('https://twitter.com/', '_blank');
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/', '_blank');
+        break;
+      case 'linkedin':
+        window.open('https://www.linkedin.com/', '_blank');
+        break;
+      case 'website':
+        window.open(window.location.href, '_blank');
+        break;
+      default:
+        break;
+    }
+  });
 });
 
-document.getElementById('twitter').addEventListener('click', function() {
-  window.open('https://twitter.com/share?url=' + encodeURIComponent(window.location.href), '_blank');
-});
-
-document.getElementById('instagram').addEventListener('click', function() {
-  window.open('https://www.instagram.com/', '_blank');
-});
-
-document.getElementById('linkedin').addEventListener('click', function() {
-  window.open('https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(window.location.href), '_blank');
-});
-
-document.getElementById('website').addEventListener('click', function() {
-  window.open(window.location.href, '_blank');
+// Add event listeners to clickable boxes
+document.querySelectorAll('.box').forEach(function(box) {
+  box.addEventListener('click', function(event) {
+    event.preventDefault();
+    var platform = this.id;
+    switch (platform) {
+      case 'facebook-box':
+        window.open('https://www.facebook.com/', '_blank');
+        break;
+      case 'twitter-box':
+        window.open('https://twitter.com/', '_blank');
+        break;
+      case 'instagram-box':
+        window.open('https://www.instagram.com/', '_blank');
+        break;
+      case 'linkedin-box':
+        window.open('https://www.linkedin.com/', '_blank');
+        break;
+      default:
+        break;
+    }
+  });
 });
